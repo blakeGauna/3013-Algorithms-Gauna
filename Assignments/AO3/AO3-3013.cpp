131a21d851615b66f734f5697fa754f32f0e4b1e
Blake Gauna <89524476+blakeGauna@users.noreply.github.com>
2022-01-18 00:06:50 -0600

    Update AO3-3013.cpp

--- Assignments/AO3/AO3-3013.cpp
+++ Assignments/AO3/AO3-3013.cpp
@@ -1,3 +1,25 @@
+/*****************************************************************************
+*                    
+*  Author:           Blake Gauna
+*  Label:            A03
+*  Title:            Linked List Class
+*  Course:           CMPS 3013
+*  Semester:         Spring 2022
+* 
+*  Description:
+*        This program implements a class that allows a linked list to be used just like 
+*        an array. It overloads the "[]" (square brackets) to simulate accessing seperate 
+*        array elements, but really it traverses the list to find the specified node using
+*        an index value. It also overloads the "+" and "<<" signs allowing a user to "add"
+*        or "push" items onto the end of the list, as well as "pop" items off the end of our 
+*        array. This class is not meant to replace the STL vector library, its simply a project
+*        to introduce the beginnings of creating complex / abstract data types. 
+*        
+*       
+*  Files:            
+*       main.cpp    : driver program 
+*****************************************************************************/
+
 #include <iostream>
 
 using namespace std;
